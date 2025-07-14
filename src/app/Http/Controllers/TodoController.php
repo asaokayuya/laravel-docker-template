a8752619d326420be3a3ea22544238f07a301389
asaoka yuya <asaoka.yy@gmail.com>
2025-07-14 10:56:35 +0900

    feat:DBにデータを保存

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Todo;
 
+use Illuminate\Http\Request; // 追記
+
 class TodoController extends Controller
 {
     public function index()
@@ -11,7 +13,7 @@ class TodoController extends Controller
         $todo = new Todo();
         $todos = $todo->all();
 
-        return view('todo.index', ['helloWorld' => 'hello World!']);
+        return view('todo.index', ['todos' => $todos]);
     }
 
     public function create()
@@ -19,4 +21,24 @@ class TodoController extends Controller
         // TODO: 第1引数を指定
         return view('todo.create'); // 追記
     }
+    public function store()
+    {
+        dd('新規作成のルート実行！');
+    }
+
+    public function store(Request $request) // 追記
+    {
+        $content = $request->input('content'); // 追記
+
+        // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
+        $todo = new Todo(); 
+        // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
+        $todo->content = $content;
+        // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+        $todo->save();
+
+        return redirect()->route('todo.index'); // 追記
+    }
+
+
 }
