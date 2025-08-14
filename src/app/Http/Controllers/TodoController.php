6449c89e2ed5094ce94bdb7adf3f3c79a792d4ca
asaoka yuya <asaoka.yy@gmail.com>
2025-08-14 14:06:18 +0900

    詳細画面・更新画面の作成

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,14 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Todo;
 
-use Illuminate\Http\Request; // 追記
+use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+    private $todo;
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
     public function index()
     {
-        $todo = new Todo();
-        $todos = $todo->all();
+        $todos = $this->todo->all();
 
         return view('todo.index', ['todos' => $todos]);
     }
@@ -26,12 +32,36 @@ class TodoController extends Controller
     {
         $inputs = $request->all(); // 変更
 
-        $todo = new Todo();
-        $todo->fill($inputs); // 変更
-        $todo->save();
+        $this->todo->fill($inputs); // 変更
+        $this->todo->save(); // 変更
 
         return redirect()->route('todo.index'); // 追記
     }
 
+    public function show($id)
+    {
+        $todo = $this->todo->find($id);
+
+        return view('todo.show', ['todo' => $todo]);
+    }
+
+    public function edit($id)
+    {
+        $todo = $this->todo->find($id);
+
+        return view('todo.edit', ['todo' => $todo]);
+    }
+
+    public function update(Request $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+    {
+        // TODO: リクエストされた値を取得
+        $inputs = $request->all();
+        // TODO: 更新対象のデータを取得
+        $todo = $this->todo->find($id);
+        // TODO: 更新したい値の代入とUPDATE文の実行
+        $todo->fill($inputs)->save();
+
+        return redirect()->route('todo.show', $todo->id);
+    }
 
 }
